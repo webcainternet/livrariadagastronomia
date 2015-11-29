980a542cf64f7f0c80c8d61a2f731aad54fc3322
fernandofmendes <fernando.mendes@webca.com.br>
2015-11-29 17:26:00 -0200

    update

---
+++ public/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://livrariadagastronomia.lojavirtual.local/admin/');
+define('HTTP_CATALOG', 'http://livrariadagastronomia.lojavirtual.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://livrariadagastronomia.lojavirtual.local/admin/');
+define('HTTPS_CATALOG', 'http://livrariadagastronomia.lojavirtual.local/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/livrariadagastronomia/public/admin/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/livrariadagastronomia/public/system/');
+define('DIR_DATABASE', '/Users/fernandomendes/github/livrariadagastronomia/public/system/database/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/livrariadagastronomia/public/admin/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/livrariadagastronomia/public/admin/view/template/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/livrariadagastronomia/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/livrariadagastronomia/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/livrariadagastronomia/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/livrariadagastronomia/public/download/');
+define('DIR_LOGS', '/Users/fernandomendes/github/livrariadagastronomia/public/system/logs/');
+define('DIR_CATALOG', '/Users/fernandomendes/github/livrariadagastronomia/public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'livrariadagastronomia');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ public/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://livrariadagastronomia.lojavirtual.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://livrariadagastronomia.lojavirtual.local/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/livrariadagastronomia/public/catalog/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/livrariadagastronomia/public/system/');
+define('DIR_DATABASE', '/Users/fernandomendes/github/livrariadagastronomia/public/system/database/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/livrariadagastronomia/public/catalog/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/livrariadagastronomia/public/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/livrariadagastronomia/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/livrariadagastronomia/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/livrariadagastronomia/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/livrariadagastronomia/public/download/');
+define('DIR_LOGS', '/Users/fernandomendes/github/livrariadagastronomia/public/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'livrariadagastronomia');
+define('DB_PREFIX', 'oc_');
+?>
