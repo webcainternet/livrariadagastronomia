d349065c5b83efe77800e2a48fbfa3d855651c89
fernandofmendes <fernando.mendes@webca.com.br>
2016-02-24 16:29:20 -0300

    ckeditor atualizado

--- public/admin/view/javascript/ckeditor/config.js
+++ public/admin/view/javascript/ckeditor/config.js
@@ -1,33 +1,37 @@
 /**
- * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For the complete reference:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-	config.filebrowserWindowWidth = '800';
+CKEDITOR.editorConfig = function(config) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+
+	config.filebrowserBrowseUrl = 'index.php?route=common/filemanager';
+	config.filebrowserImageBrowseUrl = 'index.php?route=common/filemanager';
+	config.filebrowserFlashBrowseUrl = 'index.php?route=common/filemanager';
+	config.filebrowserUploadUrl = 'index.php?route=common/filemanager';
+	config.filebrowserImageUploadUrl = 'index.php?route=common/filemanager';
+	config.filebrowserFlashUploadUrl = 'index.php?route=common/filemanager';
+
+	config.filebrowserWindowWidth = '850';
 	config.filebrowserWindowHeight = '500';
-	config.resize_enabled = false;
-	config.htmlEncodeOutput = false;
-	config.entities = false;
+
+	config.enterMode = CKEDITOR.ENTER_BR;
+	config.shiftEnterMode = CKEDITOR.ENTER_P;
+
 	config.extraPlugins = 'codemirror';
 	config.codemirror_theme = 'rubyblue';
-	config.toolbar = 'Custom';
-
-	config.toolbar_Custom = [
-		['Source'],
-		['Maximize'],
-		['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-		['NumberedList','BulletedList','-','Outdent','Indent'],
-		['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-		['SpecialChar'],
-		'/',
-		['Undo','Redo'],
-		['Font','FontSize'],
-		['TextColor','BGColor'],
-		['Link','Unlink','Anchor'],
-		['Image','Table','HorizontalRule']
-	];
+	config.codemirror = {
+		lineNumbers: true,
+		highlightActiveLine: false,
+		enableSearchTools: true,
+		showSearchButton: true,
+		showFormatButton: true,
+		showCommentButton: true,
+		showUncommentButton: true,
+		showAutoCompleteButton: true,
+		showTrailingSpace: true
+	};
 };
